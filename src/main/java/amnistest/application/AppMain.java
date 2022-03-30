c579af14bdab89f92581e01632f8adccb124b6a2
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2022-03-30 18:23:50 +0200

    Add two modules to connect to GItLab and Jira

--- src/main/java/amnistest/application/AppMain.java
+++ src/main/java/amnistest/application/AppMain.java
@@ -1,13 +1,24 @@
 package amnistest.application;
 
+import org.gitlab4j.api.GitLabApiException;
+
 public class AppMain {
     public static void main(String[] args){
 
         JiraClient jiraClient = new JiraClient("256629@student.pwr.edu.pl", "WtRdJblWpMcl2L7eDpyS8198", "https://project-amnis.atlassian.net/");
 
-
-        //jiraClient.getIssueComment("AP-6");
-        //jiraClient.getProjectURI("AP");
+        jiraClient.getIssueComment("AP-6");
+        jiraClient.getProjectURI("AP");
         jiraClient.getProjectIssueFields("AP-1");
+
+
+        GitLabClient gitLabClient = new GitLabClient();
+        try{
+            gitLabClient.getCommitHistory();
+            gitLabClient.getUser();
+        } catch (GitLabApiException exception){
+            exception.getMessage();
+        }
+
     }
 }
